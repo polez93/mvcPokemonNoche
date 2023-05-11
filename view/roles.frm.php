e792eacb3c5024c34f31d6ba11a3009833893207
Carlos Lopez <lopez.edu@yandex.ru>
2023-05-10 19:12:20 -0500

    Primer Commit MVC

---
+++ view/roles.frm.php
@@ -0,0 +1,17 @@
+<?php include_once "header.php"; ?>
+<div class="row">
+    <h1 class="text-center">Formulario de roles</h1>
+    <div class="col-3 d-flex justify-content-end aling">
+        <span class="fw-bolder">Nombre rol:</span>
+    </div>
+    <div class="col-7">
+        <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+    </div>
+</div>
+<div class="row mt-5 bg-darx">
+    <h1></h1>
+</div>
+<?php include_once "footer.php"; ?>
